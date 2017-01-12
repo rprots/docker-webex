d0565f0ba33696394991e6991d1c277d0b063b4b
Roman Prots <rprots@gmail.com>
2017-01-12 13:38:31 +0200

    Workaround to use with default docker.

--- webex.sh
+++ webex.sh
@@ -4,7 +4,7 @@ docker rm --force webex >/dev/null 2>&1
 docker build --tag=webex .
 docker run -ti \
 	--env DISPLAY=unix$DISPLAY \
-	--lxc-conf='lxc.cgroup.devices.allow = c 116:* rwm' \
+	--privileged \
 	--name=webex \
 	--volume /dev/snd:/dev/snd \
 	--volume /tmp/.X11-unix:/tmp/.X11-unix \

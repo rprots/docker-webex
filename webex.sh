094072603d120186fa50e8f38e5e4363f31fa214
rprots <rprots@gmail.com>
2017-01-12 13:50:48 +0200

    Fixed usage.

--- webex.sh
+++ webex.sh
@@ -2,9 +2,9 @@
 
 docker rm --force webex >/dev/null 2>&1
 docker build --tag=webex .
-docker run -ti \
+docker run -t \
 	--env DISPLAY=unix$DISPLAY \
-	--lxc-conf='lxc.cgroup.devices.allow = c 116:* rwm' \
+	--privileged \
 	--name=webex \
 	--volume /dev/snd:/dev/snd \
 	--volume /tmp/.X11-unix:/tmp/.X11-unix \
